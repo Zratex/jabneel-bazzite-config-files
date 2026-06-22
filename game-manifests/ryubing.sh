2601b00e0b889de39da6e51d0cf8fb635ad52fdc
Zratey <Zratey.AzErTy@gmail.com>
2026-06-23 01:10:12 +0200

    Hotfix : Ryujinx was unable to read ROM files format

--- game-manifests/ryubing.sh
+++ game-manifests/ryubing.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+ROM="$1"
+
 ~/jabneel-bazzite-config-files/set-color-platform/set-rgb.sh ns1
 
-flatpak run io.github.ryubing.Ryujinx --fullscreen "${filePath}"
+flatpak run io.github.ryubing.Ryujinx --fullscreen "$ROM"
 
 ~/jabneel-bazzite-config-files/set-color-platform/set-rgb.sh idle
