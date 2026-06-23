cab8d0fd545238f3e20d728728ea8f70dd713228
Zratey <Zratey.AzErTy@gmail.com>
2026-06-23 14:47:19 +0200

    Resetting every RGB devices so it can be controlled properly

--- set-color-platform/idle-on-startup.sh
+++ set-color-platform/idle-on-startup.sh
@@ -1,18 +1,21 @@
-# Launcement automatique du script set-rgb au démarrage :
+# Automatic restart of OpenRGB (otherwise commands won't work properly) :
 mkdir -p ~/.config/autostart
 
 cat > ~/.config/autostart/openrgb.desktop << 'EOF'
 [Desktop Entry]
 Type=Application
 Name=OpenRGB
-Exec=openrgb --startminimized
+Exec=openrgb --startminimized --server
 StartupNotify=false
 EOF
 
-cat > ~/.config/autostart/openrgb-idle.desktop << 'EOF'
-[Desktop Entry]
-Type=Application
-Name=OpenRGB Startup Set Idle
-Exec=bath -c 'sleep 6 && ~/jabneel-bazzite-config-files/set-color-platform/set-rgb.sh idle'
-StartupNotify=false
-EOF
+# Taking control of each devices by first changing the mode :
+for i in $(opnergb --list-devices | grep -oP '^\d+(?=:)'); do
+    openrgb --device $i --mode breathing
+done
+# Then reapplying static color by cutting color before hitting the idle mode
+for i in $(opnergb --list-devices | grep -oP '^\d+(?=:)'); do
+    openrgb --device $i --mode static --color 000000
+done
+# Changing to the idle color defined by user :
+~/jabneel-bazzite-config-files/set-color-platform/set-rgb.sh idle
